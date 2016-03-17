6f2a85f414fb73597b9ebb6f7ad784d074ce9220
Benjamin Blanc <benjamin@botify.com>
2016-03-17 19:16:00 +0100

    Fix typo

--- challenges/python/main.py
+++ challenges/python/main.py
@@ -5,10 +5,10 @@ from test import isPalyndrome
 class TestPalindrome(unittest.TestCase):
 
     def test_function(self):
-        for line in open('../palindrome', 'r'):
+        for line in open('./palindrome', 'r'):
             str_to_test, _is_a_palindrome = line.split('\t')
             is_a_palindrome = _is_a_palindrome == 'T\n'
-            self.assertEqual(isPalyndrome(str_to_test), is_a_palindrome)
+            self.assertEqual(isPalindrome(str_to_test), is_a_palindrome)
             print str_to_test
 
 if __name__ == '__main__':
