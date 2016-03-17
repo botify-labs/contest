fa60b939810b83a246fd26c7bec5c393ffa3955f
Benjamin Blanc <benjamin@botify.com>
2016-03-17 18:52:45 +0100

    Use unittest

--- challenges/python/main.py
+++ challenges/python/main.py
@@ -1,8 +1,14 @@
+import unittest
+
 from test import isPalyndrome
 
-for line in open('../palindrome', 'r'):
-    str_to_test, _is_a_palindrome = line.split('\t')
-    is_a_palindrome = _is_a_palindrome == 'T\n'
+class TestPalindrome(unittest.TestCase):
 
-    if isPalyndrome(str_to_test) != is_a_palindrome:
-        raise ValueError("'{}' is {}a palindrome.", str, '' if is_a_palindrome else 'not ')
+    def test_function(self):
+        for line in open('../palindrome', 'r'):
+            str_to_test, _is_a_palindrome = line.split('\t')
+            is_a_palindrome = _is_a_palindrome == 'T\n'
+            self.assertEqual(isPalyndrome(str_to_test), is_a_palindrome)
+
+if __name__ == '__main__':
+    unittest.main()
