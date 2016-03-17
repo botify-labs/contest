d3d0cede06ad18b0a30650bf975523f73a09cdfb
Benjamin Blanc <benjamin@botify.com>
2016-03-17 19:34:31 +0100

    Show str, add colors

--- challenges/python/main.py
+++ challenges/python/main.py
@@ -2,14 +2,23 @@ import unittest
 
 from test import isPalindrome
 
+FAIL = '\033[91m'
+OKGREEN = '\033[92m'
+ENDC = '\033[0m'
+
 class TestPalindrome(unittest.TestCase):
 
     def test_function(self):
         for line in open('./palindrome', 'r'):
             str_to_test, _is_a_palindrome = line.split('\t')
             is_a_palindrome = _is_a_palindrome == 'T\n'
-            self.assertEqual(isPalindrome(str_to_test), is_a_palindrome)
-            print str_to_test
-
+            print "{}{}{}".format(OKGREEN, str_to_test, ENDC)
+            try:
+                self.assertEqual(isPalindrome(str_to_test), is_a_palindrome)
+            except Exception as e:
+                print "{}{}{}".format(FAIL, str_to_test, ENDC)
+                self.assertEqual(isPalindrome(str_to_test), is_a_palindrome)
+                
 if __name__ == '__main__':
     unittest.main()
+
