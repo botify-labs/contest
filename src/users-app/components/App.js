f64d677b70867c1e383fd9ade8601244c146bbea
Nicolas Fortin <nicolas.fortin37@gmail.com>
2016-03-17 13:32:10 +0100

    Display user code

--- src/users-app/components/App.js
+++ src/users-app/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AceEditor from 'react-ace-async';
 import { getUsers } from '../resources/api';
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -11,16 +12,19 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       users: [],
+      selectedUser: null,
     };
   }
 
   componentDidMount() {
     getUsers().then(users => {
       users.push({
+        id: -1,
         name: 'toto',
         email: 'boo',
         language: 'python',
         time: 30,
+        code: 'dzqzdzqqdz',
       });
       this.setState({
         users,
@@ -28,8 +32,15 @@ export default class App extends React.Component {
     });
   }
 
+  selectUser(user) {
+    this.setState({
+      selectedUser: user,
+    });
+  }
+
   render() {
-    const { users } = this.state;
+    const { users, selectedUser } = this.state;
+
     const usersByLanguage = users.reduce((res, user) => {
       if (!res[user.language]) {
         res[user.language] = []; // eslint-disable-line no-param-reassign
@@ -44,11 +55,12 @@ export default class App extends React.Component {
         <div className="App-users">
           {languages.map(language =>
             <div
+              key={language}
               className={`App-users-${language}`}
               style={{ width: `${100 / languages.length}%` }}
             >
               <h2>{language}</h2>
-              <table className="table table-striped App-users-table">
+              <table className="App-users-table table table-hover">
                 <thead>
                   <tr>
                     <th>Name</th>
@@ -57,8 +69,12 @@ export default class App extends React.Component {
                   </tr>
                 </thead>
                 <tbody>
-                {usersByLanguage[language].map((user, i) =>
-                  <tr key={`${user.name}-${i}`}>
+                {usersByLanguage[language].map(user =>
+                  <tr
+                    key={user.id}
+                    className={selectedUser && selectedUser.id === user.id && 'active'}
+                    onClick={() => this.selectUser(user)}
+                  >
                     <td>{user.name}</td>
                     <td>{user.email}</td>
                     <td>{user.time} ms</td>
@@ -69,6 +85,16 @@ export default class App extends React.Component {
             </div>
           )}
         </div>
+        {selectedUser &&
+          <AceEditor
+            className="App-userCode form-control"
+            id="App-userCode"
+            theme="monokai"
+            mode={selectedUser.language}
+            value={selectedUser.code}
+            tabSize={2}
+          />
+        }
       </div>
     );
   }
