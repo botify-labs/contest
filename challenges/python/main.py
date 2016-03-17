1396c419f263755c3bbb2976e2d930023b0435d7
Benjamin Blanc <benjamin@botify.com>
2016-03-17 18:17:55 +0100

    Add main files

---
+++ challenges/python/main.py
@@ -0,0 +1,8 @@
+from test import isPalyndrome
+
+for line in open('../palindrome', 'r'):
+    str_to_test, _is_a_palindrome = line.split('\t')
+    is_a_palindrome = _is_a_palindrome == 'T'
+
+    if isPalyndrome(str_to_test) != is_a_palindrome:
+        raise ValueError("'{}' is {}a palindrome.", str, '' if is_a_palindrome else 'not ')
